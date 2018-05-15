0f1474b6d7bb2099859025d88e2c9547e70512f7
turnipsoup <jeremy.heckt@gmail.com>
2018-05-15 16:33:32 -0700

    ready for production of frontend

--- new_tool.js
+++ new_tool.js
@@ -26,7 +26,8 @@ function clicker() {
           if (split_list[i].includes("/tcp")) {
             if (split_list[i].split(" ").includes("open")) {
               var link_create = "http://" + $('.test-box').val() + ":" + split_list[i].split("/")[0]
-              $(".links-info").append("<a href='" + link_create + "'></a>")
+              $(".links").append("<a href='" + link_create + "'>"+ link_create + "</a>")
+              console.log("<a href='" + link_create + "'>"+ link_create + "</a>")
             }
 
           }
